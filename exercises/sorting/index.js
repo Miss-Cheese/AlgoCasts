fbe0d138382ae4cbe9a48bd6eb4da89e51d68bbd
Miss-Cheese <nat.galligan@gmail.com>
2020-04-13 19:34:32 -0400

    merging two sorted arrays

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -38,6 +38,17 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
+    const results = []
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift())
+        } else {
+            results.push(right.shift())
+        }
+    }
+
+    return [...results, ...left, ...right]
 
 }
 

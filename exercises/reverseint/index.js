41bcd2a5a73c2ba51ba5d6430e0478fbeedde4ff
Miss-Cheese <nat.galligan@gmail.com>
2020-04-15 18:05:01 -0400

    integer reversal done

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+
+
+function reverseInt(n) {
+
+   const reversedNumber = n.toString().split('').reverse().join('')
+   return parseInt(reversedNumber) * Math.sign(n)
+   
+}
 
 module.exports = reverseInt;
